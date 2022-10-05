17c2e90104a9d4014d71940bf939f90fa2aec2a1
clouden90 <clouden90@hotmail.com>
2022-10-05 07:34:45 -0400

    fix bug

--- docker/recipe/run_toy.sh
+++ docker/recipe/run_toy.sh
@@ -23,7 +23,7 @@ cd build
 cmake ..
 
 # test ufs-wm build
-ctest docker_build_ufs
+ctest -R docker_build_ufs
 
 #
 if [ $? -ne 0 ]; then
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # staging data
-ctest docker_stage_ufs_data
+ctest -R docker_stage_ufs_data
 
 #
 if [ $? -ne 0 ]; then
